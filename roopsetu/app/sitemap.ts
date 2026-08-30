f834fca3dcf821af8cf569f58620431b7c4769a4
Janu1410 <gohiljanu2003@gmail.com>
2026-08-30 22:39:34 +0530

    fixed robots and sitemap for static export

--- roopsetu/app/sitemap.ts
+++ roopsetu/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getArticleSlugs } from '@/lib/mdx';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://roopsetu.com';
 
